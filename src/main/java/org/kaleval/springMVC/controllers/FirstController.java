7678569dfe98399522889aa00b826835834ef0f0
AleVKra <aleVKra01@gmail.com>
2024-12-02 20:39:09 +0300

    6. Параметры GET запроса. Аннотация @RequestParam.вместо строкового запроса

--- src/main/java/org/kaleval/springMVC/controllers/FirstController.java
+++ src/main/java/org/kaleval/springMVC/controllers/FirstController.java
@@ -4,27 +4,19 @@ package org.kaleval.springMVC.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
-//    @GetMapping("/hello")
-//    public String helloPage() {
-//        return "first/hello";
-//    }
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
 
         System.out.println("Hello, "+ name + " "+ surname);
         return "first/hello";
-//        в строке запроса
-//        http://localhost:8080/first/hello?name=Tom&surname=Jone
     }
 
     @GetMapping("/goodbye")
